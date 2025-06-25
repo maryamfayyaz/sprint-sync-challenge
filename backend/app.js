673bf99b440eab7da62cce1bbb9cbdc81e732457
Maryam Fayyaz <maryamfayyaz1164@gmail.com>
2025-06-25 16:53:38 +0500

    chore: initialize backend project with Express and Prisma

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, (err) => {
+  console.log(`🚀 Server running on port ${PORT}`);
+    if (err) {
+        console.error("❌ Server error:", err);
+    }
+});
